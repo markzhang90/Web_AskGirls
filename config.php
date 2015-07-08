3e3145920599759ca5febce94e1d3d38a7be1f23
Yiyuan Zhang <markzhang90@gmail.com>
2015-07-08 16:25:57 -0400

    fixed

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mark
+ * Date: 5/5/15
+ * Time: 6:43 PM
+ */
+use lib\Config;
+// DB Config
+Config::write('db.host', '50.87.248.112');
+Config::write('db.port', '3306');
+Config::write('db.basename', 'xiaoruir_askgirls');
+Config::write('db.user', 'xiaoruir_mark');
+Config::write('db.password', 'Zyy45612301');
+// Project Config
+Config::write('path', 'http://localhost/Web_AskGirls/');
